db3f4d74a8a380f12ce25559dc66d45ab268ae2e
Nazar Bondarenko <nazar.bondarenko@stud.viko.lt>
2025-02-04 11:33:33 +0200

    User entity + db settings

---
+++ backend/src/main/java/com/accenture/backend/entities/User.java
@@ -0,0 +1,39 @@
+package com.accenture.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import com.accenture.backend.model.Role;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = new Timestamp(System.currentTimeMillis());
+    }
+}
