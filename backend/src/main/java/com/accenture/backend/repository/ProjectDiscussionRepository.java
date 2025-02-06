b1481f0d359ad94f85f539cbcbb1fda77b558dc9
Olena <chichevica@gmail.com>
2025-02-06 13:03:19 +0200

    Revert "Topic-Issues (Discussion Forum) feature"
    This reverts commit f70afceb9508818bbd6fa396339d9719719f2ccc.

--- backend/src/main/java/com/accenture/backend/repository/ProjectDiscussionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.accenture.backend.repository;
-
-import com.accenture.backend.model.ProjectDiscussion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectDiscussionRepository extends JpaRepository<ProjectDiscussion, Integer> {
-}
